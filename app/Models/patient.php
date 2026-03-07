bea53cadcca48a234ad23349630a56eb7e4c1569
Eslamsaber28 <sabereslam819@gmail.com>
2026-03-08 01:10:09 +0200

    edit the model Patient

--- app/Models/patient.php
+++ app/Models/patient.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class patient extends Model
+class Patient extends Model
 {
     //
     // Mass assignable attributes
