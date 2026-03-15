69c78b15b48ff8dbbac0749563db0c16a5f3cdf7
Eslamsaber28 <sabereslam819@gmail.com>
2026-03-16 01:20:32 +0200

    update lab routes

--- routes/api/lab.php
+++ routes/api/lab.php
@@ -1,12 +1,27 @@
 <?php
 
-use App\Http\Controllers\Api\Lab\SupportController;
 use App\Http\Controllers\Api\Lab\CaseController;
+use App\Http\Controllers\Api\Lab\ClinicCaseController;
+use App\Http\Controllers\Api\Lab\ClinicController;
+use App\Http\Controllers\Api\Lab\ClinicExternalController;
+use App\Http\Controllers\Api\Lab\ClinicInviteController;
+use App\Http\Controllers\Api\Lab\ClinicPartnershipController;
+use App\Http\Controllers\Api\Lab\MaterialController;
+use App\Http\Controllers\Api\Lab\SupportController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('lab')
     ->middleware(['auth:sanctum', 'role:lab'])
     ->group(function () {
+        Route::prefix('clinics')->group(function () {
+            Route::get('/', [ClinicController::class, 'index']);
+            Route::post('/invite', [ClinicInviteController::class, 'store']);
+            Route::post('/external', [ClinicExternalController::class, 'store']);
+            Route::get('/{clinic}', [ClinicController::class, 'show']);
+            Route::get('/{clinic}/cases', [ClinicCaseController::class, 'index']);
+            Route::delete('/{clinic}/partnership', [ClinicPartnershipController::class, 'destroy']);
+        });
+
         Route::prefix('cases')->group(function () {
             Route::get('/', [CaseController::class, 'index']);
             Route::post('/', [CaseController::class, 'store']);
@@ -18,6 +33,16 @@ Route::prefix('lab')
             Route::post('/{id}/messages', [CaseController::class, 'storeMessage']);
             Route::post('/{id}/attachments', [CaseController::class, 'storeAttachment']);
             Route::get('/{id}/activity-log', [CaseController::class, 'activityLog']);
+        });
+
+        Route::prefix('materials')->group(function () {
+            Route::get('/', [MaterialController::class, 'index']);
+            Route::get('/low-stock', [MaterialController::class, 'lowStock']);
+            Route::get('/expiring', [MaterialController::class, 'expiring']);
+            Route::post('/', [MaterialController::class, 'store']);
+            Route::get('/{material}', [MaterialController::class, 'show']);
+            Route::post('/{material}', [MaterialController::class, 'update']);
+            Route::delete('/{material}', [MaterialController::class, 'destroy']);
         });
 
         Route::prefix('support')->group(function () {
