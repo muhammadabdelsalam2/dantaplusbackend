183ef20e7feaba4e543655e312b9981698bde7d6
Eslamsaber28 <sabereslam819@gmail.com>
2026-05-21 16:05:20 +0300

    test the debug-orders

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+});
+
+Route::get('/debug-orders', function () {
+    return [
+        'order_count' => App\Models\Order::where('clinic_id', 26)->count(),
+        'material_count' => App\Models\MaterialOrder::where('clinic_id', 26)->count(),
+        'first_order' => App\Models\Order::where('clinic_id', 26)->first(),
+        'with_relation' => App\Models\Order::with('supplierCompany')->where('clinic_id', 26)->first(),
+    ];
 });
