93b8ce6929c461656e1ec5d6617ed3d51a2a1ab2
Eslamsaber28 <sabereslam819@gmail.com>
2026-06-15 19:28:16 +0300

    feat:testmatrial

--- app/Http/Controllers/Api/Lab/LabSelectController.php
+++ app/Http/Controllers/Api/Lab/LabSelectController.php
@@ -9,6 +9,7 @@ use App\Models\Patient;
 use App\Models\User;
 use App\Support\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class LabSelectController extends Controller
 {
